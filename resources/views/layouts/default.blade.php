9e6a7bc62b913354f157414e5f4d4cf1b850eb7b
wcs1010270451 <296952503@163.com>
2018-07-21 00:54:50 +0800

    home 0721 commit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,20 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title','laravel wcs')</title>
-  </head>
-  <body>
-    @yield('content')
-  </body>
+    <head>
+        <title>@yield('title', '艾泽拉斯') - 国家地理</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        {{--头部--}}
+        @include('layouts._header')
+
+        {{--内容--}}
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10">
+                @yield('content')
+                {{--底部--}}
+                @include('layouts._footer')
+            </div>
+        </div>
+    </body>
 </html>
