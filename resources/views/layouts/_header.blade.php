3d45ba6ca9546a98ccaeb27add7e2528ecad2510
wcs1010270451 <296952503@163.com>
2018-07-22 23:22:04 +0800

    home 0722 commit

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,12 +3,12 @@
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
             <a href="/" id="logo">艾泽拉斯</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{route('help')}}">帮助</a></li>
+                    <li><a href="#">登录</a></li>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{route('help')}}">帮助</a></li>
-                <li><a href="#">登录</a></li>
-            </ul>
-        </nav>
     </div>
 </header>
